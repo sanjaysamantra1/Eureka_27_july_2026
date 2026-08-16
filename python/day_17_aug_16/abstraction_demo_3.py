c60e1c2bbcaa9dcde912430636940bf6e0cef640
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-08-16 21:38:46 +0530

    abstraction

---
+++ python/day_17_aug_16/abstraction_demo_3.py
@@ -0,0 +1,23 @@
+
+from abc import ABC, abstractmethod
+
+class Notification(ABC):
+    @abstractmethod
+    def send(self,message):
+        pass
+    
+    
+class EmailNotification(Notification):
+    def send(self,message):
+        print(f"Sending email: {message}")
+        
+class SMSNotification(Notification):
+    def send(self,message):
+        print(f"Sending email: {message}")
+        
+        
+notification1 = EmailNotification()
+notification2 = SMSNotification()
+
+notification1.send("Your Order has been shipped")
+notification2.send("You should receive your order soon")
