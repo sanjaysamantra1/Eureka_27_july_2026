0025a1101cb642bbfae0f1bd02304a33909120ea
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-08-25 08:46:20 +0530

    directives

--- angular/angular_project_1/src/app/components/body/body.ts
+++ angular/angular_project_1/src/app/components/body/body.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { Databinding } from '../databinding/databinding';
+import { DirectivesDemo } from '../directives-demo/directives-demo';
 
 @Component({
   selector: 'app-body',
-  imports: [Databinding],
+  imports: [
+    Databinding,
+    DirectivesDemo
+  ],
   templateUrl: './body.html',
   styleUrl: './body.css',
 })
