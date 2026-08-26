39006c1e9448465e5e3d59e2764e516be26ffade
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-08-26 21:27:15 +0530

    task list

--- angular/angular_project_1/src/app/components/body/body.ts
+++ angular/angular_project_1/src/app/components/body/body.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { Databinding } from '../databinding/databinding';
 import { DirectivesDemo } from '../directives-demo/directives-demo';
+import { TaskList } from '../task-list/task-list';
 
 @Component({
   selector: 'app-body',
   imports: [
-    Databinding,
-    DirectivesDemo
+    // Databinding,
+    // DirectivesDemo
+    TaskList
   ],
   templateUrl: './body.html',
   styleUrl: './body.css',
