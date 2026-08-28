4c9b71c72dabec48b498118e8f78ba289a1d658e
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-08-28 21:34:16 +0530

    custom directive

--- angular/angular_project_1/src/app/components/products/products.ts
+++ angular/angular_project_1/src/app/components/products/products.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import product_data from './product_data';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faStar } from '@fortawesome/free-solid-svg-icons'; 
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 import {NgxPaginationModule} from 'ngx-pagination';
+import Swal from 'sweetalert2';
+import Snackbar from 'awesome-snackbar';
+import { CharOnly } from '../../custom_directives/char-only';
 
 @Component({
   selector: 'app-products',
-  imports: [FontAwesomeModule, NgxPaginationModule],
+  imports: [
+    FontAwesomeModule,
+    NgxPaginationModule,
+    CharOnly
+  ],
   templateUrl: './products.html',
   styleUrl: './products.css',
 })
@@ -14,4 +21,13 @@ export class Products {
   productArr = product_data;
   faStar = faStar;
   p: number = 1; // currentPage
+
+  openAlert(){
+    Swal.fire('Good job!', 'You clicked the button!', 'success');
+  }
+  openSnackBar(){
+    new Snackbar('Helloooo, Good Morning',
+      { position: 'top-center', theme: 'light', timeout: 5000, actionText: 'X' }
+    );
+  }
 }
---
+++ angular/angular_project_1/src/app/constants/message_constants.ts
@@ -0,0 +1,1 @@
+export let employee_added = 'Employee Added Successfully!';
