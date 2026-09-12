007bb46ad21d9c5898447d86fe60752a7828a33b
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-09-12 07:31:05 +0530

    exhaust map

--- angular/angular_project_1/src/app/components/observable-demo3/observable-demo3.ts
+++ angular/angular_project_1/src/app/components/observable-demo3/observable-demo3.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
-import { Observable, switchMap, map } from 'rxjs';
+import { Observable, switchMap, map, exhaustMap, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-observable-demo3',
@@ -25,5 +25,17 @@ export class ObservableDemo3 {
       switchMap((term) => this.http.get<any>(`https://dummyjson.com/users/search?q=${term}`)),
       map((response: any) => (response.users.length > 0 ? response.users : [])),
     );
+  }
+  // -------Exhaust map------------
+
+ @ViewChild('loginBtn') loginBtn!: ElementRef;
+ 
+ ngAfterViewInit() {
+      fromEvent(this.loginBtn.nativeElement, 'click').pipe(exhaustMap((val) => {
+        return this.http.get('https://httpbin.org/delay/5')
+      })).subscribe({
+        next: (res) => console.log('Response:', res),
+        error: (err) => console.error('Error:', err),
+      })
   }
 }
