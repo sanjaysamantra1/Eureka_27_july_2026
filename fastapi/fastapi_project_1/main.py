67782bb83ad46f3842224cd4b09e4913c1135b48
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-09-16 23:01:41 +0530

    sept 16

--- fastapi/fastapi_project_1/main.py
+++ fastapi/fastapi_project_1/main.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
 from routes.employee_routes import router as employee_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://127.0.0.1:5500"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(employee_router) # register the Routes
